bbb59fea09ded0bb98aae865485902359dc22949
Twometer <twometer@outlook.de>
2021-06-25 12:27:27 +0200

    v1.2.3: More fixes

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,14 @@
 import {getReasonPhrase} from 'http-status-codes';
 import {Response} from 'express'
 
-export class Utils {
-
-    /**
-     * Writes an error the given express response. If no error description
-     * is provided, the function selects an appropriate one from the default
-     * HTTP error descriptions
-     * @param resp  The express response
-     * @param code  The status code of the error
-     * @param desc  Optional: The error description
-     */
-    static error(resp: Response, code: number, desc?: string) {
-        return resp.status(code).send({error: desc || getReasonPhrase(code)})
-    }
-
+/**
+ * Writes an error the given express response. If no error description
+ * is provided, the function selects an appropriate one from the default
+ * HTTP error descriptions
+ * @param resp  The express response
+ * @param code  The status code of the error
+ * @param desc  Optional: The error description
+ */
+export function error(resp: Response, code: number, desc?: string) {
+    return resp.status(code).send({error: desc || getReasonPhrase(code)})
 }
